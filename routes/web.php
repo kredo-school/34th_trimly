74c19ad7e1957407620d5d71e02f6ac0bfe191d3
Yumiko Ide <yumiko_ide@yahoo.co.jp>
2025-07-28 01:16:56 +0900

    Create the register page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/login-petowner', function () {
     return view('pet_owner.login');
 });
 
+Route::get('/register-petowner', function () {
+    return view('pet_owner.register.register');
+});
+
 Route::get('/login', function () {
     return view('salon_owner/login');
 });
