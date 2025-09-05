e51f67fa6dd2d0eaeb5e004da1fe6251ff979959
Yumiko Ide <yumiko_ide@yahoo.co.jp>
2025-09-05 21:25:09 +0900

    no message

--- resources/views/pet_owner/register/header.blade.php
+++ resources/views/pet_owner/register/header.blade.php
@@ -1,29 +1,12 @@
-    <header class="bg-white shadow-sm mb-2 custom-app-header">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
-            <div class="logo">
-                <img src="{{ asset('images/Trimly Logo.png') }}" alt="Trimly Logo">
-                <p class="fw-bold text-muted mb-0 fs-5">Trimly</p>
-            </div>
-            <div class="text-end">
-                <p class="text-muted mb-0 me-2 fs-6">Pet Owner Registration</p>
-            </div>
+    <header class="bg-white mb-2 custom-app-header">
+    <div class="d-flex align-items-center justify-content-between w-100" style="padding-left:0;padding-right:0;">
+        <div class="logo d-flex align-items-center">
+            <img src="{{ asset('images/Trimly Logo.png') }}" alt="Trimly Logo">
+            <span class="fw-bold mb-0 fs-5 ms-2">Trimly</span>
         </div>
-    </header>
 
-    <style>
-        .logo {
-            display: flex;
-            align-items: center;
-            gap: 10px;
-        }
-
-        .logo img {
-            height: 80px;
-            width: auto;
-        }
-
-        .logo-text {
-            font-size: 20px;
-            font-weight: 700;
-        }
-    </style>
+        <div>
+            <p class="text-muted mb-0 fs-6">Pet Owner Registration</p>
+        </div>
+    </div>
+</header>
