4d46ceebc263986fa62ef0c82548e8fb9c03f9d7
Yumiko Ide <yumiko_ide@yahoo.co.jp>
2025-09-10 19:19:06 +0900

    I made a slight changes for header.

--- resources/views/mypage/header/mypage.blade.php
+++ resources/views/mypage/header/mypage.blade.php
@@ -4,7 +4,7 @@
         <div class="d-flex align-items-center w-100" style="padding-left:0;padding-right:0;">
             {{-- <a class="navbar-brand d-flex align-items-center p-0" href="#"> --}}
                 <div class="logo">
-                    <img src="{{ asset('images/Trimly Logo.png') }}" alt="Trimly Logo">
+                    <img src="{{ asset('images/Trimly Logo.png') }}" alt="Trimly Logo" >
                     <span class="logo-text">Trimly</span>
                 </div>
             {{-- </a> --}}
